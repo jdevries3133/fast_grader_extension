16e0ab9c5dba4b84bf9cc0c935f07b4acbfe23b9
Jack DeVries <jdevries3133@gmail.com>
2021-11-03 23:35:11 -0400

    feat: add unit tests for content script

--- src/content.ts
+++ src/content.ts
@@ -10,38 +10,36 @@ export async function getParentTable(n_retries = 0): Promise<Element> {
     throw new Error("cannot find parent table");
   }
 
-  return new Promise(async (resolve, reject) => {
-    let possibleTables = document.querySelectorAll('[aria-label="Students"]');
-    if (possibleTables.length === 0) {
-      try {
-        await wait(500);
-        return resolve(await getParentTable(n_retries + 1));
-      } catch (e) {
-        const msg = "failed to get parent table";
-        logToBackend(msg);
-        reject(msg);
-      }
-    }
-    if (possibleTables.length === 1) {
-      return resolve(possibleTables[0]);
-    }
-    // fallthrough means we have an unexpected number of tables and need to
-    // get word to the backend, because the extension is broken.
-    const msg = [
-      `expected zero or one tables, but received ${possibleTables.length}.`,
-    ];
+  let possibleTables = document.querySelectorAll('[aria-label="Students"]');
+  if (possibleTables.length === 0) {
     try {
-      // try to send string representations of the elements for additional context
-      const stringRepresentations = [];
-      for (let i = 0; i < possibleTables.length; i++) {
-        const el = <HTMLElement>possibleTables[i].cloneNode(false);
-        stringRepresentations.push(el.outerHTML);
-      }
-      msg.push(`tables: ${stringRepresentations.join(", ")}`);
-    } catch (e) {}
-    logToBackend(msg.join(", "));
-    reject(new Error(msg.join(" ")));
-  });
+      await wait(500);
+      return await getParentTable(n_retries + 1);
+    } catch (e) {
+      const msg = "failed to get parent table";
+      logToBackend(msg);
+      throw new Error(msg);
+    }
+  }
+  if (possibleTables.length === 1) {
+    return possibleTables[0];
+  }
+  // fallthrough means we have an unexpected number of tables and need to
+  // get word to the backend, because the extension is broken.
+  const msg = [
+    `expected zero or one tables, but received ${possibleTables.length}.`,
+  ];
+  try {
+    // try to send string representations of the elements for additional context
+    const stringRepresentations = [];
+    for (let i = 0; i < possibleTables.length; i++) {
+      const el = <HTMLElement>possibleTables[i].cloneNode(false);
+      stringRepresentations.push(el.outerHTML);
+    }
+    msg.push(`tables: ${stringRepresentations.join(", ")}`);
+  } catch (e) {}
+  logToBackend(msg.join(", "));
+  throw new Error(msg.join(" "));
 }
 
 async function performSync() {}
--- src/test.content.ts
+++ src/test.content.ts
@@ -1,4 +1,17 @@
+import { wait } from "./util";
 import { getParentTable } from "./content";
+import { logToBackend } from "./api";
+
+jest.mock("./util");
+jest.mock("./api");
+
+// placate typescript
+const _mockWait = <any>wait;
+const mockWait = <jest.Mock<typeof wait>>_mockWait;
+
+const { wait: originalWait } = jest.requireActual("./util");
+
+mockWait.mockImplementation((n) => originalWait(Math.floor(n / 4)));
 
 describe("getParentTable", () => {
   const getMockTable = () => {
@@ -15,6 +28,10 @@ describe("getParentTable", () => {
       });
   };
 
+  afterEach(() => {
+    document.getElementsByTagName("html")[0].innerHTML = "";
+  });
+
   it("finds the table", async () => {
     insertTables(1);
     const res = await getParentTable().catch((e) => {
@@ -23,19 +40,25 @@ describe("getParentTable", () => {
     expect(res).toContainHTML('<table aria-label="Students>');
   });
 
-  it("retries if the table is missing for up to 1000ms", async () => {
+  it("retries if the table is missing at first", async () => {
     // the func will be called before the table has been created
-    setTimeout(() => insertTables(1), 1000);
+    setTimeout(() => insertTables(1), 200);
 
     // nonetheless, the result should be accurate due to internal re-trying
-    const res = await getParentTable().catch((e) => {
-      throw new Error(e);
-    });
+    const res = await getParentTable();
     expect(res).toContainHTML('<table aria-label="Students>');
   });
 
   it("resolves to an error and logs the error if there are too many tables", async () => {
     insertTables(3);
     await expect(getParentTable()).rejects.toThrow();
+  });
+
+  it("stops recursing after 5 tries, and throws an error", async () => {
+    // @ts-ignore
+    mockWait.mockImplementation(async () => null);
+
+    await expect(getParentTable()).rejects.toThrow();
+    expect(logToBackend).toHaveBeenCalledWith("failed to get parent table");
   });
 });
